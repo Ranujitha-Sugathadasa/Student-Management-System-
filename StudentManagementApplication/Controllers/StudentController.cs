443c4544cda03a966faaf3afa5d65fc70fe28efb
Ranujitha-Sugathadasa <119477432+Ranujitha-Sugathadasa@users.noreply.github.com>
2024-07-19 23:54:15 +0530

    Update StudentController.cs

--- StudentManagementApplication/Controllers/StudentController.cs
+++ StudentManagementApplication/Controllers/StudentController.cs
@@ -6,52 +6,52 @@ using StudentManagementApplication.Models;
 
 namespace StudentManagementApplication.Controllers
 {
-    public class StudentController : Controller
-    {
-        private readonly ApplicationDbContext _context;
+    // public class StudentController : Controller
+    // {
+    //     private readonly ApplicationDbContext _context;
 
-        public StudentController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+    //     public StudentController(ApplicationDbContext context)
+    //     {
+    //         _context = context;
+    //     }
 
-        public async Task<IActionResult> Index()
-        {
-            var students = await _context.Students.Include(s => s.Courses).ToListAsync();
-            return View(students);
-        }
+    //     public async Task<IActionResult> Index()
+    //     {
+    //         var students = await _context.Students.Include(s => s.Courses).ToListAsync();
+    //         return View(students);
+    //     }
 
-        public IActionResult Create()
-        {
-            return View();
-        }
+    //     public IActionResult Create()
+    //     {
+    //         return View();
+    //     }
 
-        [HttpPost]
-        public async Task<IActionResult> Create(Student student)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(student);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(student);
-        }
+    //     [HttpPost]
+    //     public async Task<IActionResult> Create(Student student)
+    //     {
+    //         if (ModelState.IsValid)
+    //         {
+    //             _context.Add(student);
+    //             await _context.SaveChangesAsync();
+    //             return RedirectToAction(nameof(Index));
+    //         }
+    //         return View(student);
+    //     }
 
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+    //     public async Task<IActionResult> Edit(int? id)
+    //     {
+    //         if (id == null)
+    //         {
+    //             return NotFound();
+    //         }
 
-            var student = await _context.Students.FindAsync(id);
-            if (student == null)
-            {
-                return NotFound();
-            }
-            return View(student);
-        }
+    //         var student = await _context.Students.FindAsync(id);
+    //         if (student == null)
+    //         {
+    //             return NotFound();
+    //         }
+    //         return View(student);
+    //     }
 
         [HttpPost]
         public async Task<IActionResult> Edit(int id, Student student)
