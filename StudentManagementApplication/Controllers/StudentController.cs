fa3bf0bf345ed774134d9199c45fd77c7e54091b
Ranujitha-Sugathadasa <119477432+Ranujitha-Sugathadasa@users.noreply.github.com>
2024-07-22 19:22:37 +0530

    Update StudentController.cs

--- StudentManagementApplication/Controllers/StudentController.cs
+++ StudentManagementApplication/Controllers/StudentController.cs
@@ -27,31 +27,31 @@ namespace StudentManagementApplication.Controllers
         }
 
     //     [HttpPost]
-    //     public async Task<IActionResult> Create(Student student)
-    //     {
-    //         if (ModelState.IsValid)
-    //         {
-    //             _context.Add(student);
-    //             await _context.SaveChangesAsync();
-    //             return RedirectToAction(nameof(Index));
-    //         }
-    //         return View(student);
-    //     }
+        public async Task<IActionResult> Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
 
-    //     public async Task<IActionResult> Edit(int? id)
-    //     {
-    //         if (id == null)
-    //         {
-    //             return NotFound();
-    //         }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-    //         var student = await _context.Students.FindAsync(id);
-    //         if (student == null)
-    //         {
-    //             return NotFound();
-    //         }
-    //         return View(student);
-    //     }
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
 
         // [HttpPost]
         // public async Task<IActionResult> Edit(int id, Student student)
